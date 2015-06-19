9b864bd3b9f7cabe70f0c5a528ffde5782805246
Jonathan Argentiero <jonathan.argentiero@gmail.com>
2015-06-19 14:02:34 +0200

    dependencies automatic install and function refractoring

--- lib/manager/install.py
+++ lib/manager/install.py
@@ -12,37 +12,74 @@ class install:
 		self.install(packagesList)
 
 	@staticmethod
-	def install(packagesList):
+	def scaffoldPackage(packageName,packageUrl):
+		packageUrlSplitted = packageUrl.split('/')
+		tempFolderName = packageName+'-'+packageUrlSplitted[len(packageUrlSplitted)-1]
+		shutil.move(tempFolderName,packageName)
+		os.remove(packageName+'.zip')
+
+	def getPackageJsonByName(self,packageName):
+		repoUrl = "http://addons.wakanda.org/rest/Addons/?&$filter=%27name%20==%20:1%27&$params=%27[%22"+packageName+"%22]%27"
+		repoResponse = urllib2.urlopen(repoUrl)
+		responseText = repoResponse.read()
+		return json.loads(responseText)
+
+	def getPackageZipUrlByGitUrl(self,packageGitUrl):
+		return packageGitUrl.replace('/tree/','/archive/') + '.zip'	
+
+	def getDependencyJsonByKey(self,depKey):
+		depRepoUrl = 'http://addons.wakanda.org/rest/Addons('+depKey+')/dependencies?$expand=dependencies'
+		depRepoResponse = urllib2.urlopen(depRepoUrl)
+		depResponseText = depRepoResponse.read()
+		return json.loads(depResponseText)
+
+	def install(self,packagesList):
 		for (i,packageName) in enumerate(packagesList):
 			print (Fore.MAGENTA + 'INSTALLING #'+str(i+1)+ Style.RESET_ALL+' Trying to retrieve package named "'+packageName+'".') 
-			repoUrl = "http://addons.wakanda.org/rest/Addons/?&$filter=%27name%20==%20:1%27&$params=%27[%22"+packageName+"%22]%27"
-			repoResponse = urllib2.urlopen(repoUrl)
-			responseText = repoResponse.read()
-			packageJson = json.loads(responseText)
+			packageJson = self.getPackageJsonByName(packageName)
 			if packageJson['__COUNT'] == 1:
-				packageUrl = packageJson['__ENTITIES'][0]['html_url'].replace('/tree/','/archive/')
-				packageVersion = packageJson['__ENTITIES'][0]['version']
-				if packageVersion is None:
-					packageVersion = "latest"
-				packageUrlSplitted = packageUrl.split('/')
-				packageUrl += '.zip'			
+				packageGitUrl = packageJson['__ENTITIES'][0]['html_url']
+				packageUrl = self.getPackageZipUrlByGitUrl(packageGitUrl)
 				print (Fore.GREEN + '200: FOUND!' + Style.RESET_ALL)+' '+packageUrl
 				try:
+					# package details
 					packageName = packageJson['__ENTITIES'][0]['name']
-					if os.path.isdir(packageName):
-						print (Fore.RED + 'DIRECTORY NOT EMPTY!' + Style.RESET_ALL)+' Extentions/'+packageName+' directory is not empty. Aborting.' 
-					else:
-						#raw_input("Press enter to continue...")
-						download(packageUrl,packageName+'.zip')
-						print (Fore.BLUE + 'EXTRACTING!' + Style.RESET_ALL)
-						with zipfile.ZipFile(packageName+'.zip', 'r') as z:
-							z.extractall()
+					packageVersion = packageJson['__ENTITIES'][0]['version']
+					if packageVersion is None:
+						packageVersion = "latest"
+					#if os.path.isdir(packageName):
+					#	print (Fore.RED + 'DIRECTORY NOT EMPTY!' + Style.RESET_ALL)+' Extentions/'+packageName+' directory is not empty. Aborting.' 
+					
+					download(packageUrl,packageName+'.zip')
+					#print (Fore.BLUE + 'EXTRACTING!' + Style.RESET_ALL)
+					with zipfile.ZipFile(packageName+'.zip', 'r') as z:
+						z.extractall()
 
-						tempFolderName = packageName+'-'+packageUrlSplitted[len(packageUrlSplitted)-1]
-						os.rename(tempFolderName,packageName)
-						#print (Fore.BLUE + 'REMOVING .ZIP!' + Style.RESET_ALL)+' '+packageName+'.zip' 
-						os.remove(packageName+'.zip')
-						print (Fore.GREEN + 'PACKAGE INSTALLED!' + Style.RESET_ALL)+' '+packageName+'@'+packageVersion
+					# os operations
+					self.scaffoldPackage(packageName,packageGitUrl)
+					print packageName+'@'+packageVersion
+
+					# dependencies
+					depPackagesJson = self.getDependencyJsonByKey(packageJson['__ENTITIES'][0]['__KEY'])
+					if depPackagesJson['dependencies']['__COUNT'] > 0:
+						for (i,depPackageJson) in enumerate(depPackagesJson['dependencies']['__ENTITIES']):
+							depJson = self.getPackageJsonByName(depPackageJson['name'])
+							depGitUrl = depJson['__ENTITIES'][0]['html_url']
+							depUrl = self.getPackageZipUrlByGitUrl(depGitUrl)
+							depName = depJson['__ENTITIES'][0]['name']
+							depVersion = depJson['__ENTITIES'][0]['version']
+							if depVersion is None:
+								depVersion = "latest"
+
+							download(depUrl,depName+'.zip')
+							#print (Fore.BLUE + 'EXTRACTING!' + Style.RESET_ALL)
+							with zipfile.ZipFile(depName+'.zip', 'r') as z:
+								z.extractall()
+
+							self.scaffoldPackage(depName,depGitUrl)
+							print '   |_'+depName+'@'+depVersion
+
+					print (Fore.GREEN + 'PACKAGE INSTALLED!' + Style.RESET_ALL)
 				except (IOError):
 					return 1
 
